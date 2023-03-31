b93d0e36cc7ec8240d9be9b3772aa0248b1ca086
Yolanda Ferreira de Souza <landacontato@gmail.com>
2023-03-31 18:02:30 -0300

    feat: initial class

---
+++ src/pages/faq.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Faq() {
+  return (
+    <div>
+      <h1>Alura Cases - FAQ</h1>
+      <Link href="/">
+        Home
+      </Link>
+    </div>
+  )
+}
